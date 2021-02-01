41b0a2e9fe9e0ceed05ca9b9a2dffad028333689
Tuur Dutoit <tuur@datacamp.com>
2021-02-01 14:45:03 +0100

    chore: Add second test case

--- index.ts
+++ index.ts
@@ -1,12 +1,24 @@
 import { EditorState, EditorView, basicSetup } from "@codemirror/basic-setup";
 import { python } from "@codemirror/lang-python";
+import { sql } from "@codemirror/lang-sql";
 
-const initialState = EditorState.create({
-  doc: "",
-  extensions: [basicSetup, python()],
-});
+const langs = {
+  python,
+  sql,
+};
 
-const view = new EditorView({
-  parent: document.getElementById("editor"),
-  state: initialState,
-});
+function createEditor(lang, doc = "") {
+  const langExt = langs[lang];
+  const initialState = EditorState.create({
+    doc,
+    extensions: [basicSetup, langExt()],
+  });
+
+  const view = new EditorView({
+    parent: document.getElementById(`editor-${lang}`),
+    state: initialState,
+  });
+}
+
+createEditor("python");
+createEditor("sql", "SELECT 'sql';");
